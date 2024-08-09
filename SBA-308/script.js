1f578247527359b6e2afe641760642d9fb242e21
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-08-09 09:49:35 -0400

    restarting the project

--- SBA-308/script.js
+++ SBA-308/script.js
@@ -1,3 +1,7 @@
+const para = document.querySelector("#para");
+para.textContent =
+  "This assessment gauges my understanding of fundamental JavaScript concepts and my ability to apply these concepts in a practical manner.";
+
 // The provided course information.
 const CourseInfo = {
   id: 451,
@@ -78,94 +82,24 @@ const LearnerSubmissions = [
 
 function getLearnerData(course, ag, submissions) {
   // #1: declare and initialize function scope variables
-  const result = []; // array that stores the result value
-  const studentScores = {}; // object that stores the student scores - learner scorers
 
-  // #2: try/catch to get error for the whole function
-  try {
-    // #3: for of loop to initialize Learner Submissions scores
-    for (const submitted of submissions) {
-      // #4: declare and initialize for of block scope variables
-      const studentId = submitted.learner_id;
-      const assignmentID = submitted.assignment_id;
-      //   const studentScore = submitted.submission.score;
-      const studentScore = Number(submitted.submission.score); // score should always be a number
-
-      //   #5: check if the student score is not less than zero
-      if (studentScore < 0) {
-        throw `Error — The student with ID ${studentId}, has score is ${studentScore} on assignment ID ${assignmentID} which is less than zero. Verify.`;
-      }
-
-      //   #6: check if there's an entry
-      if (!studentScores[studentId]) {
-        studentScores[studentId] = {
-          id: studentId,
-          totalScore: 0,
-          totalPossiblePoints: 0,
-          scores: {},
-        };
-      }
-
-      //   #7: now search through all the assignments group and find a match
-      //   for that given assignment id
-      let assignment = null;
-      for (let i = 0; ag.assignments.length; i++) {
-        // #8: check if the AssignmentGroup's assignments ID at a certain position is the same as
-        // the LearnerSubmissions assignment ID
-        if (ag.assignments[i] === assignmentID) {
-          // assignment found
-          assignment = ag.assignments[i];
-          // #9: now, since we have the assignment we wanted, let's break and exit the loop
-          break;
-          // #10: if there' no assignment throw an error message
-        } else if (!assignment) {
-          throw `Error - Assignment with ID ${assignmentID} for student id ${studentId} is not found.`;
-        }
-      }
-
-      // #11: declare and initialize possible points
-      const possiblePoints = assignment.points_possible;
-
-      //   #12: store each score
-      studentScores[studentId].scores[assignmentID] =
-        studentScore / possiblePoints;
-
-      // #13: updatte the total score and total possible points
-      studentScores[studentId].totalScore += studentScore;
-      studentScores[studentId].totalPossiblePoints += possiblePoints;
-    }
-
-    // #14: since we have all we need to calculate the average score, let's do that
-    //   we will use the for in loop
-    for (const id of studentScores) {
-      const student = studentScores[id];
-      const avg = student.totalScore / student.totalPossiblePoints;
-
-      //   #15: built the object that combine the whole result output
-    }
-  } catch (err) {
-    console.error(`Error — ${err}`);
-  }
-
-  // here, we would process this data to achieve the desired result.
-  //   const result = [
-  //     {
-  //       id: 125,
-  //       avg: 0.985, // (47 + 150) / (50 + 150)
-  //       1: 0.94, // 47 / 50
-  //       2: 1.0, // 150 / 150
-  //     },
-  //     {
-  //       id: 132,
-  //       avg: 0.82, // (39 + 125) / (50 + 150)
-  //       1: 0.78, // 39 / 50
-  //       2: 0.833, // late: (140 - 15) / 150
-  //     },
-  //   ];
-
-  //   return result;
 }
 
-const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+// console.log(result);
 
-console.log(result);
+// here, we would process this data to achieve the desired result.
+//   const result = [
+//     {
+//       id: 125,
+//       avg: 0.985, // (47 + 150) / (50 + 150)
+//       1: 0.94, // 47 / 50
+//       2: 1.0, // 150 / 150
+//     },
+//     {
+//       id: 132,
+//       avg: 0.82, // (39 + 125) / (50 + 150)
+//       1: 0.78, // 39 / 50
+//       2: 0.833, // late: (140 - 15) / 150
+//     },
+//   ];
