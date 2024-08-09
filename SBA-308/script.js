73b9bda766157dff7ff4b6f4792532b8028ac10c
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-08-09 12:10:35 -0400

    update script.js

--- SBA-308/script.js
+++ SBA-308/script.js
@@ -77,27 +77,16 @@ const LearnerSubmissions = [
 ];
 
 function getLearnerData(course, ag, submissions) {
-  // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0, // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833, // late: (140 - 15) / 150
-    },
-  ];
+  // create the result 
+  const result = [];
+  
 
-  // return result;
+
+  return result;
 }
 
-
-// const result = [
+// here, we would process this data to achieve the desired result.
+//  const result = [
 //   {
 //     id: 125,
 //     avg: 0.985, // (47 + 150) / (50 + 150)
@@ -112,5 +101,7 @@ function getLearnerData(course, ag, submissions) {
 //   },
 // ];
 
-// const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-// console.log(result);
+// return result;
+
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+console.log(result);
