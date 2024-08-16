ad2a39be4d5d5ffc81c93e8b8bcbe42f0014e0f9
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-08-16 17:36:21 -0400

    getting started with the project

---
+++ SBA-316/scripts/index.js
@@ -0,0 +1,14 @@
+// array of mages
+const images = [
+  "https://images.pexels.com/photos/768125/pexels-photo-768125.jpeg",
+];
+
+// global scope variable
+const SectHero = document.querySelector(".hero");
+
+// set the background image of section hero
+SectHero.style.backgroundImage = `url(${images[0]})`;
+// SectHero.style.backgroundRepeat = "no-repeat";
+SectHero.style.backgroundSize = "100%";
+
+console.log(SectHero);
